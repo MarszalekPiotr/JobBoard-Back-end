faae13694e80446c36d03f1520227614ae4baafc
MarszalekPiotr <marszalekpiotr183@gmail.com>
2024-02-01 19:47:37 +0100

    Logs configuration

--- JobBoard.WebApi/Program.cs
+++ JobBoard.WebApi/Program.cs
@@ -1,10 +1,35 @@
+using Serilog;
+
 namespace JobBoard.WebApi
 {
     public class Program
     {
+        public static string _APP_NAME = "JobBoard.WebApi";
         public static void Main(string[] args)
         {
+
+            //Add loggs to console
+
+            Log.Logger = new LoggerConfiguration()
+                  .Enrich.WithProperty("Application", _APP_NAME)
+                  .Enrich.WithProperty("MacineName", Environment.MachineName)
+                  .Enrich.FromLogContext()
+                  .WriteTo.Console()
+                  .CreateBootstrapLogger();
+
+
             var builder = WebApplication.CreateBuilder(args);
+
+
+            // Add SerLog configuration to DI container
+            builder.Host.UseSerilog((context, services, configuration) => configuration
+            .Enrich.WithProperty("Application", _APP_NAME)
+            .Enrich.WithProperty("MacineName", Environment.MachineName)
+            .ReadFrom.Configuration(context.Configuration)
+            .ReadFrom.Services(services)
+            .Enrich.FromLogContext()
+            );
+
 
             // Add services to the container.
 
