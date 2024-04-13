a3f3b2d4a47137b20ffdf5aeb917ad21007d852c
MarszalekPiotr <marszalekpiotr183@gmail.com>
2024-04-13 13:11:50 +0200

    Create User Command with handler Create User Controller Base Controller

---
+++ JobBoard.Application/Logic/Users/CreateUserCommand.cs
@@ -0,0 +1,55 @@
+﻿using JobBoard.Application.Exceptions;
+using JobBoard.Application.Interfaces;
+using JobBoard.Application.Logic.Abstractions;
+using JobBoard.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobBoard.Application.Logic.Users
+{
+    public static class CreateUserCommand
+    {   
+        public class Request : IRequest<Result>
+        {
+            public required string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class Result
+        {
+            public required int UserId { get; set; }
+        }
+
+        public class Handler : BaseCommandHandler, IRequestHandler<Request, Result>
+        {
+            private IPasswordManager _passwordManager;
+            public Handler(ICurrentAccountProvider currentAccountProvider, IApplicationDbContext applicationDbContext, IPasswordManager passwordManager) : base(currentAccountProvider, applicationDbContext)
+            {
+                _passwordManager = passwordManager;
+            }
+
+            public async Task<Result> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var userExists = await _applicationDbContext.Users.AnyAsync(user => user.Email == request.Email);
+                if (userExists)
+                {
+                    throw new ErrorException("User With this Email already exists");
+                }
+                var user = new User() {  Email = request.Email,HashedPassword ="" };
+                var hashedPassword = _passwordManager.HashPassword(request.Password);
+                user.HashedPassword = hashedPassword;
+                await _applicationDbContext.Users.AddAsync(user);
+                await _applicationDbContext.SaveChangesAsync();
+                var result = new Result() { UserId = user.Id };
+                return result;
+          
+                
+            }
+        }
+    }
+}
---
+++ JobBoard.WebApi/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using JobBoard.Application.Logic.Users;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobBoard.WebApi.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class UserController : BaseController
+    {
+        public UserController(ILogger logger, IMediator mediator) : base(logger, mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateUser([FromBody] CreateUserCommand.Request model)
+        {
+            var createUserResult = await _mediator.Send(model);
+            return Ok(createUserResult); 
+        }
+    }
+}
